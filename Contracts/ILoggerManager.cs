d153b9ee04e9fa9b6832c224eaa23be8c1f78d7d
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-07-29 18:38:52 +0100

    Added Logger Service and initial configuration in the start up file

---
+++ Contracts/ILoggerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface ILoggerManager
+    {
+        void LogInfo(string message);
+        void LogWarn(string message);
+        void LogDebug(string messgae);
+        void LogError(string message);
+    }
+}
---
+++ EmployeeRegister/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private ILoggerManager _logger1;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ILoggerManager logger1)
+        {
+            _logger = logger;
+            _logger1 = logger1;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("log-info")]
+        public IEnumerable<string> GetLogInfo()
+        {
+            _logger1.LogInfo("Here is an info message from our values controller.");
+            _logger1.LogDebug("Here is an info message from our values controller.");
+            _logger1.LogWarn("Here is an warning message from our values controller.");
+            _logger1.LogError("Here is an error message from our values controller.");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
