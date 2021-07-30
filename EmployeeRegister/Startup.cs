5321ca84567713db4b97c9441c81c4896d7655a3
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-07-30 01:55:23 +0100

    Adding the Employee and Company Entities

--- EmployeeRegister/Startup.cs
+++ EmployeeRegister/Startup.cs
@@ -2,18 +2,11 @@ using EmployeeRegister.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using NLog;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EmployeeRegister
 {
