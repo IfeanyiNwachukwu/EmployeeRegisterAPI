4806abba9810d500ea0bda4a8373d12eee945e2e
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-08-05 13:57:44 +0100

    completed the addition of validation filters to all Input routes

---
+++ EmployeeRegister/Filters/ActionFilters/ValidateCompanyExistsAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.Filters.ActionFilters
+{
+    public class ValidateCompanyExistsAttribute : IAsyncActionFilter
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+
+        public ValidateCompanyExistsAttribute(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var trackChanges = context.HttpContext.Request.Method.Equals("PUT"); // track changes is set to true only if it is a PuT request, so changes are not tracked for Delete requests
+            var id = (Guid)context.ActionArguments["id"];
+            var company = await _repository.Company.GetCompanyAsync(id, trackChanges);
+
+            if(company == null)
+            {
+                _logger.LogInfo($"Company with id: {id} doesn't exist in the database");
+                context.Result = new NotFoundResult();
+            }
+            else
+            {
+                context.HttpContext.Items.Add("company", company);  //entity found in the database is stored in HttpContext because we need that entity in our action methods and we don't want to query the database twice
+                await next();
+            }
+        }
+        // An async version of the action filter is used here because we fetch our entities in an async manner
+    }
+}
---
+++ EmployeeRegister/Filters/ActionFilters/ValidateEmployeeForCompanyExists.cs
@@ -0,0 +1,50 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegister.Filters.ActionFilters
+{
+    public class ValidateEmployeeForCompanyExists : IAsyncActionFilter
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+
+        public ValidateEmployeeForCompanyExists(IRepositoryManager repository,ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var method = context.HttpContext.Request.Method;
+            var trackChanges = (method.Equals("PUT") || method.Equals("PATCH")) ? true : false;  // track changes is set to True if only it is a PUT or PATCH method
+
+            var companyId = (Guid)context.ActionArguments["companyId"];
+            var company = await _repository.Company.GetCompanyAsync(companyId, false);
+
+            if(company == null)
+            {
+
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database");
+                context.Result = new NotFoundResult();
+                return;
+            }
+            var id = (Guid)context.ActionArguments["id"];
+            var employee = await _repository.Employee.GetEmployeeAsync(companyId, id, trackChanges);
+            if(employee == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't eist in the database.");
+                context.Result = new NotFoundResult();
+            }
+            else
+            {
+                context.HttpContext.Items.Add("employee", employee);
+                await next();
+            }
+        }
+    }
+}
